bea58ba15f0a1c7cd36531bd7468031a5d7b18f3
damarion989 <d.butlers282@gmail.com>
2026-09-19 21:47:57 -0500

    Implement main method to test ConfigurationManager
    Demonstrate singleton pattern with ConfigurationManager.

--- Main.java
+++ Main.java
@@ -1,1 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        ConfigurationManager first =
+                ConfigurationManager.getInstance();
 
+        ConfigurationManager second =
+                ConfigurationManager.getInstance();
+
+        System.out.println("Same object: " + (first == second));
+
+        System.out.println("Original volume: " + second.getVolume());
+
+        first.setVolume(80);
+        System.out.println("Volume through second: " + second.getVolume());
+
+        first.setResolution("2560x1440");
+        System.out.println("Resolution through second: "
+                + second.getResolution());
+
+        first.setFullscreen(true);
+        System.out.println("Fullscreen through second: "
+                + second.isFullscreen());
+    }
+}
