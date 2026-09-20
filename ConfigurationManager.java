c37cc1480547d87e5d925b43d4084180e92acf25
damarion989 <d.butlers282@gmail.com>
2026-09-19 21:43:48 -0500

    Update ConfigurationManager.java

--- ConfigurationManager.java
+++ ConfigurationManager.java
@@ -1,1 +1,45 @@
+public class ConfigurationManager {
 
+  private static final ConfigurationManager instance = 
+        new ConfigurationManager(); 
+
+  private int volume = 50; 
+  private String resolution = "1920x1080";
+  private boolen fullscreen = false;
+
+  private ConfigurationManager() {
+  }
+
+  public static ConfigurationManager getInstance() {
+    return instance;
+  }
+
+
+  public intVolume() {
+    return Volume;
+  }
+
+  public void setVolume(int volume) {
+    if (volume < 0 | | volume > 100) {
+      throw new IllegalArgumentException(
+        " Volume must be between 0 and 100.");
+  }
+    this.volume = volume;
+  }
+
+   public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+    public boolean isFullscreen() {
+        return fullscreen;
+    }
+
+    public void setFullscreen(boolean fullscreen) {
+        this.fullscreen = fullscreen;
+    }
+}
