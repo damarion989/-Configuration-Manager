4936f420f36a1faf9759457f29dd3535388faca5
damarion989 <d.butlers282@gmail.com>
2026-09-19 21:59:54 -0500

    Implement render method in GraphicsRenderer class

--- GraphicsRenderer.java
+++ GraphicsRenderer.java
@@ -1,1 +1,17 @@
+public class GraphicsRenderer {
+    public void render() {
+        ConfigurationManager settings =
+                ConfigurationManager.getInstance();
 
+        System.out.println("Rendering at " + settings.getResolution());
+
+        if (settings.isFullscreen()) {
+            System.out.println("Fullscreen is on");
+        } else {
+            System.out.println("Fullscreen is off");
+        }
+
+        System.out.println("Volume shown on screen: "
+                + settings.getVolume());
+    }
+}
