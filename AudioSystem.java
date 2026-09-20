de2c504ab56ab84a733994f73055ea687f1deced
damarion989 <d.butlers282@gmail.com>
2026-09-19 21:59:18 -0500

    Implement AudioSystem class with sound playback

--- AudioSystem.java
+++ AudioSystem.java
@@ -1,1 +1,13 @@
+public class AudioSystem {
+    public void playSound() {
+        ConfigurationManager settings =
+                ConfigurationManager.getInstance();
 
+        System.out.println("Playing sound at volume "
+                + settings.getVolume());
+    }
+
+    public void changeVolume(int volume) {
+        ConfigurationManager.getInstance().setVolume(volume);
+    }
+}
